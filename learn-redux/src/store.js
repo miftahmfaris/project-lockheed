53e8c75a511a0fc2c731ff7f66a114ef883cfa7d
miftah faris <himeprojects@gmail.com>
2020-01-31 14:04:32 +0700

    add redux-logger

--- learn-redux/src/store.js
+++ learn-redux/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import counter from "./reducers/counter";
 import square from "./reducers/square";
 import github from "./reducers/github";
 
 export default createStore(
     combineReducers({ counter, square, github }),
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
 );
