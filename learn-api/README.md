3bd9117226e91a087ec3d799ee08a2006f97be95
miftah faris <himeprojects@gmail.com>
2020-02-05 19:20:54 +0700

    learn api

---
+++ learn-api/README.md
@@ -0,0 +1,9 @@
+# Learn API
+
+This repository for learn an api with express framework
+
+1 . `git clone` url
+
+2. `yarn install`
+
+3. `yarn start`
---
+++ learn-api/index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const todos = require("./todos");
+
+const PORT = process.env.PORT;
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome to my API", data: todos });
+});
+
+app.get("/hello", (req, res) => {
+    res.send({
+        message: "This is hello route"
+    });
+});
+
+app.get("/hello/:id", (req, res) => {
+    const { id } = req.params;
+
+    res.send({
+        message: `This is hello route with params ${id}`
+    });
+});
+
+app.post("/", (req, res) => {
+    const { id, todo } = req.body;
+
+    todos.push({ id: parseInt(id), todo });
+
+    res.send({ message: "Your data is added", data: todos });
+});
+
+app.listen(PORT, () => {
+    console.log(`My API is listen on PORT: ${PORT}`);
+});
