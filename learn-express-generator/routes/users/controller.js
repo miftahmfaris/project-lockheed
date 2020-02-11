f9ea4969383b03d5c1f975d0468965073fd8c876
miftah faris <himeprojects@gmail.com>
2020-02-11 12:00:55 +0700

    add bcrypt library

--- learn-express-generator/routes/users/controller.js
+++ learn-express-generator/routes/users/controller.js
@@ -1,4 +1,5 @@
 const { Users } = require("../../models");
+const { hashPassword, comparedPassword } = require("../../helpers");
 
 module.exports = {
     getAll: async (req, res) => {
@@ -29,16 +30,41 @@ module.exports = {
         try {
             const data = req.body;
             const file = req.file;
+            const hash = await hashPassword(req.body.password);
 
             const result = await Users.create({
                 ...data,
-                avatar: file === undefined ? null : file.path
+                avatar: file === undefined ? null : file.path,
+                password: hash
             });
 
             res.status(200).send({
                 message: "New data user is successfully added",
                 data: result
             });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const result = await Users.findOne({ email: req.body.email });
+
+            const compared = await comparedPassword(
+                req.body.password,
+                result.password
+            );
+
+            if (compared === true) {
+                res.status(200).send({
+                    message: "You are successfully logged in",
+                    data: result
+                });
+            } else {
+                res.status(403).send({
+                    message: "You are not an user, please register first"
+                });
+            }
         } catch (error) {
             console.log(error);
         }
--- learn-express-generator/routes/users/index.js
+++ learn-express-generator/routes/users/index.js
@@ -5,5 +5,6 @@ const { upload } = require("../../config");
 router.get("/", require("./controller").getAll);
 router.get("/id/:id", require("./controller").getById);
 router.post("/", upload.single("avatar"), require("./controller").postData);
+router.post("/login", require("./controller").login);
 
 module.exports = router;
