8de7c222c2a35725a2153a7763f7e8c7dddcf528
miftah faris <himeprojects@gmail.com>
2020-02-12 15:02:17 +0700

    add route update, add todos

--- learn-mysql/routes/todos/index.js
+++ learn-mysql/routes/todos/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", require("./controller").getAll);
+router.post("/", require("./controller").addData);
+router.put("/:id", require("./controller").updateOne);
 
 module.exports = router;
